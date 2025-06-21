03077f8d00968112863d702d401c4d42f854dbe5
Jame007-spec2 <nawaphat.k@psru.ac.th>
2025-06-21 19:19:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 27017
 const productRoute = require('./routes/productRoute')
 const morgan = require('morgan')
 const { readdirSync } = require('fs')
