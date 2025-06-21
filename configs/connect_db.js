a1f718a8ed237a5dbcb6bea93ddbf992e8742675
Jame007-spec2 <nawaphat.k@psru.ac.th>
2025-06-22 06:56:11 +0700

    Update connect_db.js

--- configs/connect_db.js
+++ configs/connect_db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const db_url = process.env.DB_URL
+const db_url = ${{ MongoDB.MONGO_URL }}
 
 exports.connectDB = async () => {
     try {
