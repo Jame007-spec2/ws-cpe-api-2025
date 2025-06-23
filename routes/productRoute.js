b7ecf53189a504f70df7907e7203a78db8bdf1b7
Jame007-spec2 <nawaphat.k@psru.ac.th>
2025-06-23 10:19:05 +0700

    Update productRoute.js

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAllProducts, addProduct,getProductsById,updateProductsById,deleteProductsById } = require('../controllers/productController');
+const {
+    getAllProducts,
+    addProduct,
+    getProductsById,
+    updateProductsById,
+    deleteProductsById }
+    = require('../controllers/productController');
 
 router.get('/getAllProducts', getAllProducts);
 router.get('/getProductsById/:id', getProductsById)
-
 router.post('/addProduct', addProduct)
 router.put('/updateProductsById/:id', updateProductsById)
-
 router.delete('/deleteProductsById/:id', deleteProductsById)
 
 module.exports = router
