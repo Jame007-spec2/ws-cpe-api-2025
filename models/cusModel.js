120a85172240d8bf422adce7606c27be542abff5
Jame007-spec2 <nawaphat.k@psru.ac.th>
2025-06-24 04:14:02 +0700

    Create cusModel.js

---
+++ models/cusModel.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+const ProductSchema = new mongoose.Schema({
+    cus_name: { type: String,require:true },
+    cus_price: { type: String,require:true },
+    cus_planet: { type: String,require:true }
+},{timestamps:true, versionKey: false})
+
+module.exports = mongoose.model('customers',ProductSchema)
